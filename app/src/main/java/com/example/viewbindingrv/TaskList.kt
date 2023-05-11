04728900da254e6ed47d69254a7e1ba9f831a3c4
HanSeokMin <tjrals604@naver.com>
2023-05-11 15:22:23 +0900

    리사이클러뷰 데모 완성

---
+++ app/src/main/java/com/example/viewbindingrv/TaskList.kt
@@ -0,0 +1,17 @@
+package com.example.viewbindingrv
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("걷기","7:00 am"),
+        Task("아침 식사","8:00 am"),
+        Task("미팅","9:00 am"),
+        Task("커피 타임","10:00 am"),
+        Task("오전 업무","11:00 am"),
+        Task("점심 식사","12:00 pm"),
+        Task("오후 미팅","13:00 pm"),
+        Task("오후 업무","14:00 pm"),
+        Task("청소","15:00 pm"),
+        Task("마무리","16:00 am"),
+
+    )
+}
